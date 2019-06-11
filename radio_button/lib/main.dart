eec7b3bcab3a8f44912c124539f2e55e9572f85c
infiniteoverflow <aswingopinathan1871@gmail.com>
2019-06-12 00:13:26 +0530

    Radio Button

---
+++ radio_button/lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var groupValue = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text("RadioButton Demo"),
+        ),
+        body: Padding(
+          padding: EdgeInsets.only(left: 20.0,top: 100.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Radio(
+                    value: 1,
+                    groupValue: groupValue,
+                    onChanged: (T) {
+                      setState(() {
+                        groupValue = T;
+                      });
+                    },
+                    activeColor: Colors.black,
+                  ),
+
+                  Text(
+                    "North Indian",
+                    style: TextStyle(
+                      fontSize: 20.0
+                    ),
+                  )
+                ],
+              ),
+
+
+              Row(
+                children: <Widget>[
+                  Radio(
+                    value: 2,
+                    groupValue: groupValue,
+                    onChanged: (T) {
+                      setState(() {
+                        groupValue = T;
+                      });
+                    },
+                    activeColor: Colors.black,
+                  ),
+
+                  Text(
+                    "Chinese",
+                    style: TextStyle(
+                        fontSize: 20.0
+                    ),
+                  )
+                ],
+              ),
+
+
+              Row(
+                children: <Widget>[
+                  Radio(
+                    value: 3,
+                    groupValue: groupValue,
+                    onChanged: (T) {
+                      setState(() {
+                        groupValue = T;
+                      });
+                    },
+                    activeColor: Colors.black,
+                  ),
+
+                  Text(
+                    "South Indian",
+                    style: TextStyle(
+                        fontSize: 20.0
+                    ),
+                  )
+                ],
+              ),
+
+            ],
+          ),
+        )
+    );
+  }
+}
