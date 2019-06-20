70c93a78eaf8b2ea8243c9b2405c3073cb96204c
infiniteoverflow <aswingopinathan1871@gmail.com>
2019-06-20 22:13:19 +0530

    Cupertino Popup Surface

--- cupertino_popup_surface/lib/main.dart
+++ cupertino_popup_surface/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -74,38 +76,38 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+        child: RaisedButton(
+          child: Text(
+            "Click ME !!"
+          ),
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) => CupertinoPopupSurface(
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 20.0),
+                    child: Text(
+                      "This is a Cupertino Popup Surface...",
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 20.0
+                      ),
+                    ),
+                  )
+                )
+              )
+            );
+          },
+
+          color: Colors.orangeAccent,
+          shape: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0)
+          ),
+          elevation: 15.0,
+          padding: EdgeInsets.only(left: 40.0,right: 40.0,top: 20.0,bottom: 20.0),
+        )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
