774d37f542c7b4be62643ef4b81d554817984163
infiniteoverflow <aswingopinathan1871@gmail.com>
2019-06-22 20:32:43 +0530

    Cupertino Tab Scaffold

---
+++ cupertino_tab_scaffold/lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int selectedTabIndex = 0;
+  IconData icon = Icons.map;
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.map),title: Text("Maps")),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle),title: Text("Account")),
+          BottomNavigationBarItem(icon: Icon(Icons.settings),title: Text("Settings")),
+        ],
+        onTap: (int index) {
+          setState(() {
+            selectedTabIndex = index;
+
+            if(index==0)
+              icon = Icons.map;
+            else if(index==1)
+              icon = Icons.account_circle;
+            else
+              icon = Icons.settings;
+          });
+        },
+        currentIndex: selectedTabIndex,
+      ),
+
+      tabBuilder: (BuildContext context,int index) {
+        return CupertinoTabView(
+          builder: (BuildContext context) {
+            return CupertinoPageScaffold(
+              navigationBar: CupertinoNavigationBar(
+                leading: Icon(Icons.arrow_back_ios),
+                middle: Text(
+                  "Cupertino Tab Scaffold"
+                ),
+                trailing: Icon(Icons.home),
+              ),
+              child: SafeArea(
+                child: Center(
+                  child: Icon(icon)
+                )
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
