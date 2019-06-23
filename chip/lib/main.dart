99120542276eaff900e793dadc12f9ae03f48c95
infiniteoverflow <aswingopinathan1871@gmail.com>
2019-06-23 21:21:33 +0530

    Chip

--- chip/lib/main.dart
+++ chip/lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Chip Demo'),
     );
   }
 }
@@ -44,18 +45,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,38 +63,61 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
+          padding: EdgeInsetsDirectional.only(top: 200.0),
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Chip(
+              label: Text(
+                "Bangalore",
+                style: TextStyle(
+                    fontSize: 25.0
+                ),
+              ),
+              elevation: 10.0,
+              avatar: CircleAvatar(
+                child: Icon(Icons.location_city),
+              ),
+              shadowColor: Colors.red,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+
+            Padding(
+              padding: EdgeInsets.all(10.0),
             ),
+
+            Chip(
+              label: Text(
+                "Chennai",
+                style: TextStyle(
+                    fontSize: 25.0
+                ),
+              ),
+              elevation: 10.0,
+              avatar: CircleAvatar(
+                child: Icon(Icons.location_city),
+              ),
+              shadowColor: Colors.green,
+            ),
+
+            Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
+
+            Chip(
+              label: Text(
+                "Delhi",
+                style: TextStyle(
+                    fontSize: 25.0
+                ),
+              ),
+              elevation: 10.0,
+              avatar: CircleAvatar(
+                child: Icon(Icons.location_city),
+              ),
+              shadowColor: Colors.amber,
+            )
           ],
-        ),
+        )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
