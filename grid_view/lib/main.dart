baca80228cec82b1af0820a34bb8b08d29e977e1
infiniteoverflow <aswingopinathan1871@gmail.com>
2019-06-25 23:22:46 +0530

    GridView

--- grid_view/lib/main.dart
+++ grid_view/lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'GridView Demo'),
     );
   }
 }
@@ -44,18 +45,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,41 +60,94 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: <Widget>[
+          Center(
+            child: RaisedButton(
+              child: Text("1"),
+              onPressed: () {
+
+              },
+              color: Colors.amber,
+              elevation: 10.0,
+            )
+          ),
+          Center(
+              child: RaisedButton(
+                child: Text("2"),
+                onPressed: () {
+
+                },
+                color: Colors.red,
+                elevation: 10.0,
+              )
+          ),
+          Center(
+              child: RaisedButton(
+                child: Text("3"),
+                onPressed: () {
+
+                },
+                color: Colors.yellow,
+                elevation: 10.0,
+              )
+          ),
+          Center(
+              child: RaisedButton(
+                child: Text("4"),
+                onPressed: () {
+
+                },
+                color: Colors.blue,
+                elevation: 10.0,
+              )
+          ),
+          Center(
+              child: RaisedButton(
+                child: Text("5"),
+                onPressed: () {
+
+                },
+                color: Colors.green,
+                elevation: 10.0,
+              )
+          ),
+          Center(
+              child: RaisedButton(
+                child: Text("6"),
+                onPressed: () {
+
+                },
+                color: Colors.brown,
+                elevation: 10.0,
+              )
+          ),
+
+          Center(
+              child: RaisedButton(
+                child: Text("7"),
+                onPressed: () {
+
+                },
+                color: Colors.amber,
+                elevation: 10.0,
+              )
+          ),
+
+          Center(
+              child: RaisedButton(
+                child: Text("8"),
+                onPressed: () {
+
+                },
+                color: Colors.red,
+                elevation: 10.0,
+              )
+          ),
+
+        ],
+      )
     );
   }
 }
